ba5fbfe20e58750231dd330f2922245bc6928779
Doorlock <kjsu1994@gmail.com>
2022-05-09 16:52:50 +0900

    액티비티 출입기록이동까지

--- app/src/main/java/iot/jinsu/smart_doorlock/activity_Main.java
+++ app/src/main/java/iot/jinsu/smart_doorlock/activity_Main.java
@@ -2,7 +2,10 @@ package iot.jinsu.smart_doorlock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_Main extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class activity_Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button button = (Button) findViewById(R.id.logButton);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent =  new Intent(activity_Main.this,
+                        activity_log.class);
+                startActivity(intent);
+            }
+        });
     }
 }
