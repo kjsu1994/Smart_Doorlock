317d56ca5a5a30d4822c680baa650ed9ce115787
zinu0320 <zinu0320@gmail.com>
2022-05-11 17:09:13 +0900

    엑티비티 메뉴들 버튼 이동까지

---
+++ app/src/main/java/iot/jinsu/smart_doorlock/ActivityOtp.java
@@ -0,0 +1,14 @@
+package iot.jinsu.smart_doorlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityOtp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
--- app/src/main/java/iot/jinsu/smart_doorlock/activity_Main.java
+++ app/src/main/java/iot/jinsu/smart_doorlock/activity_Main.java
@@ -15,6 +15,9 @@ public class activity_Main extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Button button = (Button) findViewById(R.id.logButton);
         Button button1 = (Button) findViewById(R.id.securityButton);
+        Button button2 = (Button) findViewById(R.id.pwchangeButton);
+        Button button3 = (Button) findViewById(R.id.otpButton);
+        Button button4 = (Button) findViewById(R.id.openButton);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -28,6 +31,30 @@ public class activity_Main extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent =  new Intent(activity_Main.this,
                         ActivitySecuremode.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent =  new Intent(activity_Main.this,
+                        ActivityPwchange.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent =  new Intent(activity_Main.this,
+                        ActivityOtp.class);
+                startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent =  new Intent(activity_Main.this,
+                        ActivityOpenDoor.class);
                 startActivity(intent);
             }
         });
